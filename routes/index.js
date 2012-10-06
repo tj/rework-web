4ecfdc06665abbd35754048b12c1cad109ec8afe
TJ Holowaychuk <tj@vision-media.ca>
2012-10-06 10:32:11 -0700

    add initial POST / route stub

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 
+/**
+ * Module dependencies.
+ */
+
+var rework = require('rework')
+  , bytes = require('bytes')
+  , fs = require('fs');
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+/**
+ * POST .file.
+ */
+
+exports.process = function(req, res, next){
+  var file = req.files.style;
+  if (!file) return res.send(400, '"style" required, try `curl -F style=@my.css`');
+  console.log('reading %s', file.path);
+  fs.readFile(file.path, 'utf8', function(err, css){
+    if (err) return next(err);
+    console.log('processing %s', bytes(css.length));
+
+  });
 };
